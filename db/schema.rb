c1ac6b20d863fb65862ec7e4095946188eb0998a
Cory Rabiea <m00tzmanAlmighty@gmail.com>
2015-05-15 19:52:16 -0400

    added name attribute to skids. implemented on fields.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150514100417) do
+ActiveRecord::Schema.define(version: 20150515233958) do
 
   create_table "forms", force: :cascade do |t|
     t.string   "name"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20150514100417) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.integer  "shipment_id"
+    t.string   "product"
   end
 
 end
